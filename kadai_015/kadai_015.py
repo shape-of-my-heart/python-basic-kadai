f13af33e24f049bf75673eacb257b513bf46a843
shape-of-my-heart <142283806+shape-of-my-heart@users.noreply.github.com>
2023-09-21 21:23:23 +0900

    Update kadai_015.py

--- kadai_015/kadai_015.py
+++ kadai_015/kadai_015.py
@@ -1,32 +1,18 @@
-{\rtf1\ansi\ansicpg932\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue255;\red245\green245\blue245;\red0\green0\blue0;
-\red101\green76\blue29;\red0\green0\blue109;\red144\green1\blue18;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c100000;\cssrgb\c96863\c96863\c96863;\cssrgb\c0\c0\c0;
-\cssrgb\c47451\c36863\c14902;\cssrgb\c0\c6275\c50196;\cssrgb\c63922\c8235\c8235;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+class Human:
 
-\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 class\cf0 \strokec4  Human:\cb1 \
-\
-\pard\pardeftab720\partightenfactor0
-\cf0 \cb3    \cf2 \strokec2 def\cf0 \strokec4  \cf5 \strokec5 __init__\cf0 \strokec4 (\cf6 \strokec6 printinfo\cf0 \strokec4 , \cf6 \strokec6 name\cf0 \strokec4 , \cf6 \strokec6 age\cf0 \strokec4 ):\cb1 \
-\cb3      printinfo.name = name\cb1 \
-\cb3      printinfo.age = age\cb1 \
-\
-\cb3    \cf2 \strokec2 def\cf0 \strokec4  \cf5 \strokec5 set_name\cf0 \strokec4 (\cf6 \strokec6 printinfo\cf0 \strokec4 , \cf6 \strokec6 name\cf0 \strokec4 , \cf6 \strokec6 age\cf0 \strokec4 ):\cb1 \
-\cb3      printinfo.name = name\cb1 \
-\cb3      printinfo.age = age\cb1 \
-\
-\cb3    \cf2 \strokec2 def\cf0 \strokec4  \cf5 \strokec5 show_name\cf0 \strokec4 (\cf6 \strokec6 printinfo\cf0 \strokec4 ):\cb1 \
-\cb3      \cf5 \strokec5 print\cf0 \strokec4 (printinfo.name)\cb1 \
-\cb3      \cf5 \strokec5 print\cf0 \strokec4 (printinfo.age)\cb1 \
-\
-\cb3 human = Human(\cf7 \strokec7 "\uc0\u21517 \u21069 "\cf0 \strokec4 ,\cf7 \strokec7 "\uc0\u24180 \u40802 "\cf0 \strokec4 )\cb1 \
-\
-\pard\pardeftab720\partightenfactor0
-\cf5 \cb3 \strokec5 print\cf0 \strokec4 (human.name)\cb1 \
-\cf5 \cb3 \strokec5 print\cf0 \strokec4 (human.age)\cb1 \
-}
+   def __init__(printinfo, name, age):
+     printinfo.name = name
+     printinfo.age = age
+
+   def set_name(printinfo, name, age):
+     printinfo.name = name
+     printinfo.age = age
+
+   def show_name(printinfo):
+     print(printinfo.name)
+     print(printinfo.age)
+
+human = Human("名前","年齢")
+
+print(human.name)
+print(human.age)

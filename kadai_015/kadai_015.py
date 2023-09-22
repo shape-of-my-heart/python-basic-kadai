b38233fa19046af60ee38d9bccc9d85d36e1d4c3
shape-of-my-heart <142283806+shape-of-my-heart@users.noreply.github.com>
2023-09-22 13:59:35 +0900

    Update kadai_015.py

--- kadai_015/kadai_015.py
+++ kadai_015/kadai_015.py
@@ -4,9 +4,9 @@ class Human:
      self.name = name
      self.age = age
 
-   def show_name(printinfo):
-     print(printinfo.name)
-     print(printinfo.age)
+   def printinfo(self):
+     print(self.name)
+     print(self.age)
 
 human = Human('太郎',20)
 
